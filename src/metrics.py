39f88fbf187f60c3907b5be4c8a0cd6ecee1d34d
FerasBasha <feras_albasha@hotmail.com>
2020-12-23 08:48:00 -0500

    updates

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 from math import sqrt
 from sklearn.metrics import r2_score
 import mlflow
@@ -66,8 +66,10 @@ def get_metrics(y_true, y_pred, param_prefix='validation'):
     rmse = sqrt(mean_squared_error(y_true, y_pred))
     r2_metric = r2_score(y_true, y_pred)
     mape_metric = mean_absolute_percentage_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
     
     return {'wape':wape,
             'rmse':rmse,
             'r2':r2_metric,
-            'mape':mape_metric}
+            'mape':mape_metric,
+            'mae':mae}
